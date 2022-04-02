cf7e7442a1fc0123449d1b143e4414b96eccb7f9
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-02 15:05:37 +0200

    feat(App): Añadir main

--- src/main/java/edu/poniperro/furnace/App.java
+++ src/main/java/edu/poniperro/furnace/App.java
@@ -2,7 +2,23 @@ package edu.poniperro.furnace;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main( String[] args )
+    {
+        final double minTemp = 15.0;
+        final double maxTemp = 21.0;
+
+        RoomTemperature temperature = new RoomTemperature(15);
+        Heater heater = new GasHeater();
+        Thermometer thermometer = new RemoteCommandSensor();
+
+        Regulator regulator = new Regulator();
+
+        System.out.println( "Arrancando..." );
+        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+
+        Jedi yoda = new Jedi();
+        System.out.println( "\nArrancando a Yoda: " );
+        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
+        yoda.speak();
     }
 }
