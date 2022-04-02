e6975b4b88f23dff236e35fbbe9fcb55110cebd9
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-02 17:24:54 +0200

    feat(jedi): Añadir funcionalidad a los métodos

--- src/main/java/edu/poniperro/furnace/higherbeings/Jedi.java
+++ src/main/java/edu/poniperro/furnace/higherbeings/Jedi.java
@@ -6,15 +6,31 @@ import edu.poniperro.furnace.interfaces.Temperature;
 public class Jedi implements Heater {
     @Override
     public void engage(Temperature temperature) {
-
+        temperature.changeTemperatureBy(useLightSable());
     }
 
     @Override
     public void disengage(Temperature temperature) {
+        temperature.changeTemperatureBy(useForce());
+    }
 
+    public int useLightSable() {
+        int lightSableTemp = 1400;
+        return lightSableTemp;
+    }
+
+    public int useForce() {
+        return -1400;
     }
 
     public void speak() {
-        System.out.println();
+        System.out.println("\n"
+                + "\t  __.-._   \n"
+                + "\t  '-._\"7' \n"
+                + "\t   /'.-c   \n"
+                + "\t   |  /T   \n"
+                + "\t  _)_/LI   \n"
+                + "\nDo or do not. There is no try ");
+
     }
 }
