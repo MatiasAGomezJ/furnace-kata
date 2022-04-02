e1c1a5c645bef69d698199373484df86c3cb8f00
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-02 15:50:44 +0200

    refactor: Comentar código no escrito

--- src/main/java/edu/poniperro/furnace/App.java
+++ src/main/java/edu/poniperro/furnace/App.java
@@ -1,31 +1,32 @@
 package edu.poniperro.furnace;
 
-import edu.poniperro.furnace.hardware.GasHeater;
-import edu.poniperro.furnace.hardware.Regulator;
-import edu.poniperro.furnace.hardware.RemoteCommandSensor;
-import edu.poniperro.furnace.interfaces.Heater;
-import edu.poniperro.furnace.interfaces.Thermometer;
-import edu.poniperro.furnace.otherstuff.Jedi;
+//import edu.poniperro.furnace.hardware.GasHeater;
+//import edu.poniperro.furnace.hardware.Regulator;
+//import edu.poniperro.furnace.hardware.RemoteCommandSensor;
+//import edu.poniperro.furnace.interfaces.Heater;
+//import edu.poniperro.furnace.interfaces.Thermometer;
+//import edu.poniperro.furnace.otherstuff.Jedi;
 import edu.poniperro.furnace.types.RoomTemperature;
 
 public class App {
 
     public static void main(String[] args) {
-        final double minTemp = 15.0;
-        final double maxTemp = 21.0;
 
-        RoomTemperature temperature = new RoomTemperature(15);
-        Heater heater = new GasHeater();
-        Thermometer thermometer = new RemoteCommandSensor();
+//        final double minTemp = 15.0;
+//        final double maxTemp = 21.0;
 
-        Regulator regulator = new Regulator();
+//        RoomTemperature temperature = new RoomTemperature(15);
+//        Heater heater = new GasHeater();
+//        Thermometer thermometer = new RemoteCommandSensor();
 
-        System.out.println("Arrancando...");
-        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+//        Regulator regulator = new Regulator();
 
-        Jedi yoda = new Jedi();
-        System.out.println("\nArrancando a Yoda: ");
-        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
-        yoda.speak();
+//        System.out.println("Arrancando...");
+//        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+
+//        Jedi yoda = new Jedi();
+//        System.out.println("\nArrancando a Yoda: ");
+//        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
+//        yoda.speak();
     }
 }
--- src/main/java/edu/poniperro/furnace/hardware/Regulator.java
+++ src/main/java/edu/poniperro/furnace/hardware/Regulator.java
@@ -1,38 +1,38 @@
 package edu.poniperro.furnace.hardware;
 
-import edu.poniperro.furnace.interfaces.Heater;
-import edu.poniperro.furnace.interfaces.Thermometer;
-import edu.poniperro.furnace.types.RegulatorDisplayCodes;
+//import edu.poniperro.furnace.interfaces.Heater;
+//import edu.poniperro.furnace.interfaces.Thermometer;
+//import edu.poniperro.furnace.types.RegulatorDisplayCodes;
 import edu.poniperro.furnace.types.RoomTemperature;
 
 public class Regulator {
 
-    public void regulate(Thermometer t, Heater h, double minTemp, double maxTemp, RoomTemperature temperature) {
-        RegulatorDisplayCodes code;
-        while (t.read(temperature) < maxTemp) {
-            code = RegulatorDisplayCodes.HEATING;
-            h.engage(temperature);
-            message(code, temperature);
-        }
-        while (t.read(temperature) > minTemp) {
-            code = RegulatorDisplayCodes.WAITING;
-            h.disengage(temperature);
-            message(code, temperature);
-        }
-    }
+//    public void regulate(Thermometer t, Heater h, double minTemp, double maxTemp, RoomTemperature temperature) {
+//        RegulatorDisplayCodes code;
+//        while (t.read(temperature) < maxTemp) {
+//            code = RegulatorDisplayCodes.HEATING;
+//            h.engage(temperature);
+//            message(code, temperature);
+//        }
+//        while (t.read(temperature) > minTemp) {
+//            code = RegulatorDisplayCodes.WAITING;
+//            h.disengage(temperature);
+//            message(code, temperature);
+//        }
+//    }
 
-    private void message(RegulatorDisplayCodes code, RoomTemperature temperature) {
-        switch (code) {
-            case HEATING:
-                System.out.println("Calentando => temperatura " + temperature.getTemperature());
-                break;
-            case WAITING:
-                System.out.println("Apagado => temperatura " + temperature.getTemperature());
-                break;
-            default:
-                System.out.println("Algo raro sucede...");
-                break;
-        }
-    }
+//    private void message(RegulatorDisplayCodes code, RoomTemperature temperature) {
+//        switch (code) {
+//            case HEATING:
+//                System.out.println("Calentando => temperatura " + temperature.getTemperature());
+//                break;
+//            case WAITING:
+//                System.out.println("Apagado => temperatura " + temperature.getTemperature());
+//                break;
+//            default:
+//                System.out.println("Algo raro sucede...");
+//                break;
+//        }
+//    }
 
 }
