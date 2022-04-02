5e0da0e4bb4e0a6749e887577bee0b2e193b581a
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-02 14:56:55 +0200

    First commit

---
+++ src/main/java/edu/poniperro/furnace/App.java
@@ -0,0 +1,8 @@
+package edu.poniperro.furnace;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/edu/poniperro/furnace/AppTest.java
@@ -0,0 +1,13 @@
+package edu.poniperro.furnace;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void trueIsTrue() {
+        assertTrue(true);
+    }
+}
