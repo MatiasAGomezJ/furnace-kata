987374165c2cc91ec53b183f8c05848cf07b6e0b
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-05 23:00:12 +0200

    refactor(roomtemperature): Aplicar uso de Optional

--- src/main/java/edu/poniperro/furnace/types/RoomTemperature.java
+++ src/main/java/edu/poniperro/furnace/types/RoomTemperature.java
@@ -2,19 +2,22 @@ package edu.poniperro.furnace.types;
 
 import edu.poniperro.furnace.interfaces.Temperature;
 
+import java.util.Optional;
+
 public class RoomTemperature implements Temperature {
 
     private int temperature;
-    private static RoomTemperature instance = null;
+    private static Optional<RoomTemperature> instance = Optional.ofNullable(null);
+
 
     private RoomTemperature() {
     }
 
     public static RoomTemperature getInstance() {
-        if (instance == null) {
-            instance = new RoomTemperature();
+        if (instance.isEmpty()) {
+            instance = Optional.of(new RoomTemperature());
         }
-        return instance;
+        return instance.get();
     }
 
     @Override
